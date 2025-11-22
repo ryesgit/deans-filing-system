bd3ec7b36a59d64c2ca5a2d3e887ff0a1680457d
Rey <100455152+codenamerey@users.noreply.github.com>
2025-11-22 10:35:42 +0800

    refactor: admin to ADMIN

--- src/DeptHeadPage/DashboardPage/sections/QuickActionCard/QuickActionCard.jsx
+++ src/DeptHeadPage/DashboardPage/sections/QuickActionCard/QuickActionCard.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 import "./style.css";
 
-export const QuickActionCard = ({ onAddFile, onRequest, onAddMember, role }) => {
+export const QuickActionCard = ({
+  onAddFile,
+  onRequest,
+  onAddMember,
+  role,
+}) => {
   return (
     <div className="quick-action-card">
       <div className="quick-action-header">
         <h3 className="quick-action-title">Quick Actions</h3>
         <p className="quick-action-instruction">
-        Lorem ipsum dolor sit amet consectetur. Risus enim.
+          Lorem ipsum dolor sit amet consectetur. Risus enim.
         </p>
       </div>
       <div className="quick-action-buttons">
         <button className="action-btn" onClick={onAddFile}>
           Add File
         </button>
-        {role === "admin" && (
+        {role === "ADMIN" && (
           <button className="action-btn" onClick={onRequest}>
             Request
           </button>
