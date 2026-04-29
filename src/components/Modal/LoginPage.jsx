eed31717965633bff135b5adadc5e38467066071
Rey <100455152+codenamerey@users.noreply.github.com>
2026-04-30 05:47:32 +0800

    Load login hero image from GCS

--- src/components/Modal/LoginPage.jsx
+++ src/components/Modal/LoginPage.jsx
@@ -4,6 +4,9 @@ import "./LoginPage.css";
 import { Modal } from "./Modal";
 import { RegistrationPage } from "./RegistrationPage";
 
+const LOGIN_HERO_IMAGE_URL =
+  "https://storage.googleapis.com/deans-filing-system-assets-463695708385/frontend/frame_2.png";
+
 export const LoginPage = () => {
   const { login, error, clearError } = useAuth();
   const [username, setUsername] = useState("");
@@ -43,7 +46,7 @@ export const LoginPage = () => {
         <RegistrationPage onClose={() => setIsRegistrationOpen(false)} />
       </Modal>
 
-      <img className="login-page-image" alt="Frame" src="/frame_2.png" />
+      <img className="login-page-image" alt="Frame" src={LOGIN_HERO_IMAGE_URL} />
 
       <div className="login-page-right">
         {error && (
